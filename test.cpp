c3473fdc689a04c2648e3ff3216f330f437f7988
Devansh Rautela <91424721+devanshrautela@users.noreply.github.com>
2023-01-31 02:20:49 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,237 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+from mediapipe.framework.formats import landmark_pb2
+import time
+from math import sqrt
+import win32api
+import pyautogui
+ 
+def count_fingers(lst):
+    cnt = 0
+ 
+    thresh = (lst.landmark[0].y*100 - lst.landmark[9].y*100)/2
 
+    if (lst.landmark[5].y*100 - lst.landmark[8].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[9].y*100 - lst.landmark[12].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[13].y*100 - lst.landmark[16].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[17].y*100 - lst.landmark[20].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[5].x*100 - lst.landmark[4].x*100) > 6:
+        cnt += 1
+
+
+    return cnt 
+ 
+video = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+start_init = False
+
+prev = -1
+ 
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands: 
+    while True: 
+        end_time  = time.time()
+        _, frame = video.read()
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+         
+        image = cv2.flip(image, 1)
+ 
+        imageHeight, imageWidth, _ = image.shape
+ 
+        results = hands.process(image)
+   
+ 
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                         )
+            hand_keyPoints = results.multi_hand_landmarks[0]
+            cnt = count_fingers(hand_keyPoints)
+            if not(prev==cnt):
+                if not(start_init):
+                    start_time = time.time()
+                    start_init = True
+
+                elif (end_time-start_time) > 0.2:
+                    if (cnt == 1): 
+                        pyautogui.press("right")
+                        print("right")
+                
+                    elif (cnt == 2):  
+                        pyautogui.press("left") 
+                        print("left")
+ 
+                    elif (cnt == 3):
+                        pyautogui.click()
+                        print("click ")
+               
+                    elif (cnt == 5):
+                        pyautogui.press("space")
+                        print("space")    
+
+                    prev = cnt
+                    start_init = False 
+ 
+            mp_drawing.draw_landmarks(image, hand_keyPoints, mp_hands.HAND_CONNECTIONS)
+
+ 
+        if results.multi_hand_landmarks != None:
+          for handLandmarks in results.multi_hand_landmarks:
+            for point in mp_hands.HandLandmark:
+ 
+
+ 
+    
+                normalizedLandmark = handLandmarks.landmark[point]
+                pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, imageWidth, imageHeight)
+    
+                point=str(point)
+ 
+                if point=='HandLandmark.INDEX_FINGER_TIP':
+                 try:
+                    indexfingertip_x=pixelCoordinatesLandmark[0]
+                    indexfingertip_y=pixelCoordinatesLandmark[1] 
+                    win32api.SetCursorPos((indexfingertip_x*4,indexfingertip_y*5))
+ 
+                 except:
+                    pass
+ 
+        cv2.imshow('Hand Tracking', image)
+        
+        
+ 
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+ 
+video.release()import mediapipe as mp
+import cv2
+import numpy as np
+from mediapipe.framework.formats import landmark_pb2
+import time
+from math import sqrt
+import win32api
+import pyautogui
+ 
+def count_fingers(lst):
+    cnt = 0
+ 
+    thresh = (lst.landmark[0].y*100 - lst.landmark[9].y*100)/2
+
+    if (lst.landmark[5].y*100 - lst.landmark[8].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[9].y*100 - lst.landmark[12].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[13].y*100 - lst.landmark[16].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[17].y*100 - lst.landmark[20].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[5].x*100 - lst.landmark[4].x*100) > 6:
+        cnt += 1
+
+
+    return cnt 
+ 
+video = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+start_init = False
+
+prev = -1
+ 
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands: 
+    while True: 
+        end_time  = time.time()
+        _, frame = video.read()
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+         
+        image = cv2.flip(image, 1)
+ 
+        imageHeight, imageWidth, _ = image.shape
+ 
+        results = hands.process(image)
+   
+ 
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                         )
+            hand_keyPoints = results.multi_hand_landmarks[0]
+            cnt = count_fingers(hand_keyPoints)
+            if not(prev==cnt):
+                if not(start_init):
+                    start_time = time.time()
+                    start_init = True
+
+                elif (end_time-start_time) > 0.2:
+                    if (cnt == 1): 
+                        pyautogui.press("right")
+                        print("right")
+                
+                    elif (cnt == 2):  
+                        pyautogui.press("left") 
+                        print("left")
+ 
+                    elif (cnt == 3):
+                        pyautogui.click()
+                        print("click ")
+               
+                    elif (cnt == 5):
+                        pyautogui.press("space")
+                        print("space")    
+
+                    prev = cnt
+                    start_init = False 
+ 
+            mp_drawing.draw_landmarks(image, hand_keyPoints, mp_hands.HAND_CONNECTIONS)
+
+ 
+        if results.multi_hand_landmarks != None:
+          for handLandmarks in results.multi_hand_landmarks:
+            for point in mp_hands.HandLandmark:
+ 
+
+ 
+    
+                normalizedLandmark = handLandmarks.landmark[point]
+                pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, imageWidth, imageHeight)
+    
+                point=str(point)
+ 
+                if point=='HandLandmark.INDEX_FINGER_TIP':
+                 try:
+                    indexfingertip_x=pixelCoordinatesLandmark[0]
+                    indexfingertip_y=pixelCoordinatesLandmark[1] 
+                    win32api.SetCursorPos((indexfingertip_x*4,indexfingertip_y*5))
+ 
+                 except:
+                    pass
+ 
+        cv2.imshow('Hand Tracking', image)
+        
+        
+ 
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+ 
+video.release()
